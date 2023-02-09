c6ebc3591edefe4e955f22f3f6909b3b8573503b
Edward Wang <ewang43@jhu.edu>
2023-02-09 15:43:40 -0500

    Implement Paddle class and draw it onto the canvas

--- brick-breaker/main.js
+++ brick-breaker/main.js
@@ -1,24 +1,36 @@
 import "./style.css";
 import Ball from "./model/Ball.js";
+import Paddle from "./model/Paddle.js";
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 const ball = new Ball(
   canvas.width / 2,
-  canvas.height-30,
+  canvas.height - 30,
   10,
   10,
   "#0095DD",
   2,
   -2
-)
+);
+
+const paddle = new Paddle(
+  (canvas.width - 10) / 2,
+  canvas.height - 10,
+  75,
+  10,
+  "#0095DD"
+);
 
 function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ball.draw(ctx);
   ball.move();
   ball.bounce(canvas.width, canvas.height);
+
+  paddle.draw(ctx);
+  paddle.move();
 
   window.requestAnimationFrame(draw);
 }
