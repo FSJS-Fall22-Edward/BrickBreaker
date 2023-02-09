214b3090bc4c565111a364099f889a404ec6a2b8
Edward Wang <ewang43@jhu.edu>
2023-02-09 15:52:56 -0500

    Create a Brick class

---
+++ brick-breaker/model/Brick.js
@@ -0,0 +1,22 @@
+import Block from "./Block.js";
+
+class Brick extends Block {
+    constructor(x, y, width, height, color) {
+        super(x,y,width,height,color);
+        this.visible = true;
+    }
+
+    draw(ctx) {
+        if(this.visible) {
+            super.draw(ctx);
+        }
+    }
+
+    collides(ball) {
+        if(this.visible && this.intersects(ball)) {
+            this.visible = false;
+            ball.collides(this);
+        }
+    }
+}
+export default Brick;
