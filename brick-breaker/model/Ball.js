42e002f3fc145eb43d2a9b4df967ba606547e3fe
Edward Wang <ewang43@jhu.edu>
2023-02-09 15:33:24 -0500

    Add ball to canvas

---
+++ brick-breaker/model/Ball.js
@@ -0,0 +1,13 @@
+import Sprite from "./Sprite.js";
+
+class Ball extends Sprite {
+    bounce(canvasWidth, canvasHeight) {
+        if(this.x < 0 || this.x+this.width > canvasWidth) {
+            this.dx *= -1;
+        } else if(this.y < 0 || this.y+this.height > canvasHeight) {
+            this.dy *= -1;
+        }
+    }
+}
+
+export default Ball;
